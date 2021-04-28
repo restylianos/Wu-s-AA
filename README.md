14a962200a296330565c8adc9172b393cefff091
lizzli <restemstelios@yahoo.com>
2021-04-28 08:12:38 -0500

    Update main

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Useful links:
-https://www.geeksforgeeks.org/anti-aliased-line-xiaolin-wus-algorithm/
-https://unionassets.com/blog/algorithm-brezenhema-and-wu-s-line-299
+# Useful links:  
+* https://www.geeksforgeeks.org/anti-aliased-line-xiaolin-wus-algorithm/
+* https://unionassets.com/blog/algorithm-brezenhema-and-wu-s-line-299
--- testing.c
+++ testing.c
@@ -8,12 +8,14 @@
 #include <stdio.h>
 
 //set global vars
-int x1, myy1, x2, y2;
+int def_x1=20, def_y1=30, def_x2=550, def_y2=150;
+//def stands for default!
+int first_x,first_y,second_x,second_y;
 
 void myInit() {
 	glClearColor( 0,0,0,0);
     glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_PROJECTION);
+	
 	gluOrtho2D(0, 640, 0, 480);
     return;
 }
@@ -25,8 +27,14 @@ void draw_pixels( int x , int y , float brightness)
     return;
 }
 
+void draw_normal_pixels(int x,int y){
+    //use y+20 to create a new simple line 20 px higher than the AA line
+    y +=20;
+    glColor3f(1,0,0);
+    glVertex2i(x,y);
+}
 
-void draw_line(int x0 , int y0 , int x1 , int y1)
+void draw_lines(int x0 , int y0 , int x1 , int y1)
 {
     glBegin(GL_POINTS);
     int steep = get_abs_num(y1 - y0) > get_abs_num(x1 - x0) ;
@@ -49,6 +57,7 @@ void draw_line(int x0 , int y0 , int x1 , int y1)
     float dx = x1-x0;
     float dy = y1-y0;
     float gradient = dy/dx;
+
     if (dx == 0.0)
         gradient = 1;
   
@@ -65,6 +74,7 @@ void draw_line(int x0 , int y0 , int x1 , int y1)
             // part of y co-ordinate
             draw_pixels(get_integer_part(intersectY), x,get_round_fract(intersectY));
             draw_pixels(get_integer_part(intersectY)-1, x, get_fract_part(intersectY));
+            draw_normal_pixels(x,intersectY);
             intersectY += gradient;
         }
     }
@@ -76,17 +86,23 @@ void draw_line(int x0 , int y0 , int x1 , int y1)
             // part of y co-ordinate
             draw_pixels(x, get_integer_part(intersectY),get_round_fract(intersectY));
             draw_pixels(x, get_integer_part(intersectY)-1,get_fract_part(intersectY));
+            draw_normal_pixels(x,intersectY);
             intersectY += gradient;
         }
     }
-  glEnd();
   return;
 }
 
 
 void myDisplay() {
-	draw_line(20 , 300 , 620, 400);
-    // draw_normal_line(20,600,250,70);
+    if(first_x && second_y){
+        draw_lines(first_x,first_y,second_x,second_y);
+    }
+    else{
+        draw_lines(def_x1,def_y1,def_x2,def_y2);
+    }
+	
+    glEnd();
 	glFlush();
     return;
 }
@@ -94,17 +110,29 @@ void myDisplay() {
 
 int main(int argc, char **argv) {
 
-	printf( "(x1,y1) = [20,30] && (x2,y2) = [550, 150]\n");
-	// scanf("%d %d %d %d", &x1, &myy1, &x2, &y2);
+	printf( "\033[32;1mUsage:\nCoordinates given via terminal: ./a.out x1,y1,x2,y2\nUsing default coordinates which are: (x1,y1) = [20,30] && (x2,y2) = [550, 150]\033[0m\n");
+    
+    if(argc > 1){
+        printf("ok");
+        first_x = strtol(argv[1],NULL,10);
+        first_y = strtol(argv[2],NULL,10);
+        second_x = strtol(argv[3],NULL,10);
+        second_y = strtol(argv[4],NULL,10);
+    }
+
+    
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(0, 0);
+	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Anti-aliased line");
-
+    glMatrixMode(GL_PROJECTION);
 	myInit();
-	glutDisplayFunc(myDisplay);
-    glPointSize(1.0f);
+    glutDisplayFunc(myDisplay);
+    
+	
+    glPointSize(2.0f);
 	glutMainLoop();
 
     return 0;
