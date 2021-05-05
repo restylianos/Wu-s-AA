90f81870a4f8f670cd5ee594c031842bbf0c57bc
lizzli <restemstelios@yahoo.com>
2021-05-05 04:59:15 -0500

    Minor fixes
    Updated main.c and helper_funs

--- helper_funs.h
+++ helper_funs.h
@@ -20,13 +20,6 @@ int get_integer_part(float input_num)
 {
     return (int)input_num;
 }
-  
-//rounds off a number
-// int get_round_number(float x)
-// {
-//     return get_integer_part(x + 0.5) ;
-// }
-  
 //returns fractional part of a num
 
 float get_fract_part(float input_num)
@@ -40,4 +33,81 @@ float get_fract_part(float input_num)
 float get_round_fract(float input_num)
 {
     return 1 - get_fract_part(input_num);
+}
+
+void draw_pixels( int x , int y , float brightness)
+{
+    glColor4f((1-brightness),(1-brightness),(1-brightness),1);
+	glVertex2i(x, y);
+    return;
+}
+
+void draw_normal_pixels(int x,int y){
+    //use y+20 to create a new simple line 20 px higher than the AA line
+    y +=20;
+    glColor3f(1,0,0);
+    glVertex2i(x,y);
+}
+
+void myInit() {
+	glClearColor( 0,0,0,0);
+    glClear(GL_COLOR_BUFFER_BIT);
+	gluOrtho2D(0, 640, 0, 480);
+    //make pointers bigger to make results more clear
+    glPointSize(5.0f);
+    return;
+}
+
+void draw_lines(int x0 , int y0 , int x1 , int y1)
+{
+    int i;
+
+    glBegin(GL_POINTS);
+
+    int slope = get_abs_num(y1 - y0) > get_abs_num(x1 - x0) ;
+    //get slope
+
+
+    // swap if slope > 1 
+
+    // draw backwards
+    if (slope){
+        swap_nums(&x0 , &y0);
+        swap_nums(&x1 , &y1);
+    }
+
+    if (x0 > x1){
+        swap_nums(&x0 ,&x1);
+        swap_nums(&y0 ,&y1);
+    }
+  
+    //compute the slope
+    float dx = x1-x0;
+    float dy = y1-y0;
+
+    float grad = dy/dx;
+
+    if (dx == 0.0) grad = 1;
+        
+    float f_y = y0;
+    //get y as float
+
+    //condition check and draw part
+    if (slope){
+        for (i=x0;i<=x1;i++){
+            draw_pixels(get_integer_part(f_y), i,get_round_fract(f_y));
+            draw_pixels(get_integer_part(f_y)-1, i, get_fract_part(f_y));
+            draw_normal_pixels(i,f_y);
+            f_y += grad;
+        }
+    }
+    else{
+        for (i=x0;i<=x1;i++){
+            draw_pixels(i, get_integer_part(f_y),get_round_fract(f_y));
+            draw_pixels(i, get_integer_part(f_y)-1,get_fract_part(f_y));
+            draw_normal_pixels(i,get_integer_part(f_y));
+            f_y += grad;
+        }
+    }
+    return;
 }
--- main.c
+++ main.c
@@ -4,12 +4,8 @@
 #include <GL/glut.h>
 #endif
 
-
-
 #include "helper_funs.h"
 #include <stdio.h>
-
-
 
 
 //set global vars
@@ -17,101 +13,8 @@ int def_x1=20, def_y1=30, def_x2=550, def_y2=150;
 //def stands for default!
 int first_x,first_y,second_x,second_y;
 
+void myDisplay();
 
-void myInit() {
-	glClearColor( 0,0,0,0);
-    glClear(GL_COLOR_BUFFER_BIT);
-	gluOrtho2D(0, 640, 0, 480);
-    //make pointers bigger to make results more clear
-    glPointSize(3.0f);
-    return;
-}
-
-void draw_pixels( int x , int y , float brightness)
-{
-    glColor4f((1-brightness),(1-brightness),(1-brightness),1);
-	glVertex2i(x, y);
-    return;
-}
-
-void draw_normal_pixels(int x,int y){
-    //use y+20 to create a new simple line 20 px higher than the AA line
-    y +=20;
-    glColor3f(1,0,0);
-    glVertex2i(x,y);
-}
-
-void draw_lines(int x0 , int y0 , int x1 , int y1)
-{
-    glBegin(GL_POINTS);
-    int steep = get_abs_num(y1 - y0) > get_abs_num(x1 - x0) ;
-    //get slope
-
-
-    // swap the co-ordinates if slope > 1 
-    
-    // draw backwards
-    if (steep){
-        swap_nums(&x0 , &y0);
-        swap_nums(&x1 , &y1);
-    }
-
-    if (x0 > x1){
-        swap_nums(&x0 ,&x1);
-        swap_nums(&y0 ,&y1);
-    }
-  
-    //compute the slope
-    float dx = x1-x0;
-    float dy = y1-y0;
-    float gradient = dy/dx;
-
-    if (dx == 0.0)
-        gradient = 1;
-  
-    int xpxl1 = x0;
-    int xpxl2 = x1;
-    float intersectY = y0;
-  
-    // main loop
-    if (steep){
-        int x;
-        for (x = xpxl1 ; x <=xpxl2 ; x++){
-            // pixel coverage is determined by fractional
-            // part of y co-ordinate
-            draw_pixels(get_integer_part(intersectY), x,get_round_fract(intersectY));
-            draw_pixels(get_integer_part(intersectY)-1, x, get_fract_part(intersectY));
-            draw_normal_pixels(x,intersectY);
-            intersectY += gradient;
-        }
-    }
-    else{
-        int x;
-        for (x=xpxl1;x<=xpxl2;x++){
-            // pixel coverage is determined by fractional
-            // part of y co-ordinate
-            draw_pixels(x, get_integer_part(intersectY),get_round_fract(intersectY));
-            draw_pixels(x, get_integer_part(intersectY)-1,get_fract_part(intersectY));
-            draw_normal_pixels(x,intersectY);
-            intersectY += gradient;
-        }
-    }
-    return;
-}
-
-
-void myDisplay() {
-    if(first_x && second_y){
-        draw_lines(first_x,first_y,second_x,second_y);
-    }
-    else{
-        draw_lines(def_x1,def_y1,def_x2,def_y2);
-    }
-	
-    glEnd();
-	glFlush();
-    return;
-}
 
 
 int main(int argc, char **argv) {
@@ -141,4 +44,18 @@ int main(int argc, char **argv) {
 	glutMainLoop();
 
     return 0;
+}
+
+void myDisplay() {
+    if(first_x && second_y){
+        draw_lines(first_x,first_y,second_x,second_y);
+    }
+    else{
+        draw_lines(def_x1,def_y1,def_x2,def_y2);
+    }
+	
+    glEnd();
+	glFlush();
+    
+    return;
 }
